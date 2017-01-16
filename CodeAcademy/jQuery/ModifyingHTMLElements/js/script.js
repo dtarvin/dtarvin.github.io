e64d47bda6334afc8c487a59d844bf9a95750296
David R Tarvin Jr <david.tarvin@gmail.com>
2017-01-16 09:18:08 -0600

    Update script.js

--- CodeAcademy/jQuery/ModifyingHTMLElements/js/script.js
+++ CodeAcademy/jQuery/ModifyingHTMLElements/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#button").click(function) {
         var toAdd = $("input[name=checkListItem]").val();
-        $("#list).append("toAdd");
+        $("#list").append("toAdd");
     });
     $(document).on('click', 'list', function() {
         $("#list").remove();
